a5ab1ed6dd54ba560f9d7bebee2765e36fba8463
mickey.wang.kangce <mickey.wang.kangce@kangce.net>
2019-08-22 13:24:59 +0800

    commit code

---
+++ common/src/main/java/util/DataType.java
@@ -0,0 +1,15 @@
+package util;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * description
+ *
+ * @author mickey
+ * 23/07/2019
+ */
+@Slf4j
+@SuppressWarnings({"RedundantCast"})
+public class DataType {
+
+}
