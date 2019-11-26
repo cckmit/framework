2c687a3b54df6af79a3c413aeb1b267fe2079988
mickey.wang.kangce <mickey.wang.kangce@kangce.net>
2019-11-26 10:36:16 +0800

    backup code

--- common/src/main/java/org/mickey/framework/common/config/SystemDateFormat.java
+++ common/src/main/java/org/mickey/framework/common/config/SystemDateFormat.java
@@ -16,23 +16,56 @@ import java.util.Date;
 @Slf4j
 public class SystemDateFormat extends DateFormat {
     private DateFormat dateFormat;
-    private SimpleDateFormat format1 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+    private static final SimpleDateFormat DATE_FORMAT_PATTERN = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+
+    public SystemDateFormat() {
+        super();
+    }
 
     public SystemDateFormat(DateFormat dateFormat) {
+        super();
         this.dateFormat = dateFormat;
+    }
+
+    public static void main(String[] args) {
+        SystemDateFormat format = new SystemDateFormat();
+
+        for(int i=0; i<20; ++i) {
+            new Thread(()->{
+                Date parse;
+                try {
+                    parse = format.parse("yyyy-MM-dd HH:mm:ss");
+                    System.out.println(parse);
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+            }).start();
+        }
+
     }
 
     @Override
     public StringBuffer format(Date date, StringBuffer toAppendTo, FieldPosition fieldPosition) {
-        return dateFormat.format(date, toAppendTo, fieldPosition);
+
+        StringBuffer result = null;
+        try {
+            result = DATE_FORMAT_PATTERN.format(date, toAppendTo, fieldPosition);
+        } catch (Exception e) {
+            result = dateFormat.format(date, toAppendTo, fieldPosition);
+        }
+
+        return result;
     }
 
     @Override
     public Date parse(String source, ParsePosition pos) {
+
         Date date = null;
         try {
-            date = format1.parse(source, pos);
-        } catch (Exception ex) {
+            date = DATE_FORMAT_PATTERN.parse(source, pos);
+        } catch (Exception e) {
             date = dateFormat.parse(source, pos);
         }
         return date;
@@ -40,10 +73,11 @@ public class SystemDateFormat extends DateFormat {
 
     @Override
     public Date parse(String source) throws ParseException {
+
         Date date = null;
         try {
-            date = format1.parse(source);
-        } catch (Exception ex) {
+            date = DATE_FORMAT_PATTERN.parse(source);
+        } catch (Exception e) {
             date = dateFormat.parse(source);
         }
         return date;
