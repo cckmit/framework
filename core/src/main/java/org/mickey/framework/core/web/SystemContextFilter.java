f5bda92fdf22c323f9111716b00797fa6e527698
mickey.surface <mickey.surface@outlook.com>
2020-02-13 14:40:57 +0800

    backup code

--- core/src/main/java/org/mickey/framework/core/web/SystemContextFilter.java
+++ core/src/main/java/org/mickey/framework/core/web/SystemContextFilter.java
@@ -38,7 +38,7 @@ public class SystemContextFilter implements Filter {
         HttpServletResponse response = (HttpServletResponse) servletResponse;
 
         try{
-            log.error("system context init");
+            log.debug("system context init");
             Enumeration<String> headerNames = request.getHeaderNames();
             while (headerNames.hasMoreElements()) {
                 String currentHeader = headerNames.nextElement();
@@ -88,6 +88,6 @@ public class SystemContextFilter implements Filter {
     }
 
     private void printSystemContextSetLog(String requestURI, String systemConstant, String headerValue) {
-        log.error("request {} has  header: {}, with value {}", requestURI, systemConstant, headerValue);
+        log.debug("request {} has  header: {}, with value {}", requestURI, systemConstant, headerValue);
     }
 }
